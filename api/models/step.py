bf40925c78907d87b7cc70fb49d0c0d1951aaea2
Audrius_spiridavicius <audriusspiridavicius@gmail.com>
2024-04-06 17:44:13 +0300

    step model null/blank false

--- api/models/step.py
+++ api/models/step.py
@@ -2,6 +2,6 @@ from django.db import models
 from api.models.task import Task
 
 class Step(models.Model):
-    name = models.CharField(max_length=100, verbose_name="step title")
+    name = models.CharField(max_length=100, verbose_name="step title" , blank=False, null=False)
 
-    tasks = models.ManyToManyField("Task", through=Task.steps.through)
+    tasks = models.ManyToManyField(Task, through=Task.steps.through)
