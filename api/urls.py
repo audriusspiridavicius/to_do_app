560460819c9188838e4a4690f0f38fceb404cba2
Audrius_spiridavicius <audriusspiridavicius@gmail.com>
2024-04-11 16:26:33 +0300

    task custom action url

--- api/urls.py
+++ api/urls.py
@@ -1,15 +1,17 @@
 from django.urls import include, path
 from rest_framework import routers
-from api.views.step import StepViewSet
+from api import views
 # from tutorial.quickstart import views
 
 router = routers.DefaultRouter()
-router.register(r'steps', StepViewSet, basename="step")
-# router.register(r'groups', views.GroupViewSet)
+router.register(r'steps', views.StepViewSet, basename="step")
+router.register(r'tasks', views.TaskViewSet, basename="task")
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
+    
+    path("tasks/name/<str:taskname>/", views.TaskViewSet.as_view({"get":"list"}), name="task-filter-name"),
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
