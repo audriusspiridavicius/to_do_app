094d4ace2fe4bf1b52fd304d90c7725a8153789c
Audrius_spiridavicius <audriusspiridavicius@gmail.com>
2024-04-25 20:18:04 +0300

    custom user admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from api.models.user import UserCustom
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
+@admin.register(UserCustom)
+class UserCustomAdmin(UserAdmin):
+   ordering = ['email']
