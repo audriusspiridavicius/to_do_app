3bee95513c09d5481165fe6c45b88c76c4beea15
Audrius_spiridavicius <audriusspiridavicius@gmail.com>
2024-06-11 17:18:48 +0300

    UserViewSet update. user details update functionality added

--- api/views/user.py
+++ api/views/user.py
@@ -1,14 +1,16 @@
 from rest_framework import viewsets
-from api.serializers.user import LoggedUserDetailsSerializer, UserSerializer
+from api.serializers.user import LoggedUserDetailsSerializer, UpdateUserDetailsSerializer, UserSerializer
 from django.contrib.auth import get_user_model
 from api.models import UserCustom
-
+from rest_framework import permissions
+from rest_framework.parsers import MultiPartParser, FormParser, JSONParser
 User=get_user_model()
 
 # Create your views here.
 
-class UserViewSet(viewsets.ReadOnlyModelViewSet):
+class UserViewSet(viewsets.ReadOnlyModelViewSet, viewsets.mixins.UpdateModelMixin):
     serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     
     def get_queryset(self):
 
@@ -25,5 +27,9 @@ class UserViewSet(viewsets.ReadOnlyModelViewSet):
         
         if user_email:
             return LoggedUserDetailsSerializer
+        
+        if self.action == "update":
+            self.serializer_class = UpdateUserDetailsSerializer
+        
         
         return super().get_serializer_class()
